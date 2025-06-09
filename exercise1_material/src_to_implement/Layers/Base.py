0867abc7e2441c142c1b3ead66504e1d85546165
unknown <vxrun2000@gmail.com>
2025-06-09 17:27:25 +0200

    Deleted unwanted files

--- exercise1_material/src_to_implement/Layers/Base.py
+++
@@ -1,5 +0,0 @@
-class BaseLayer: 
-    def __init__(self): #Constructor with  no arguments
-        self.trainable = False #Set trainable to false
-        self.optimizer = None
-        self.weights = None #Default weight parameter
